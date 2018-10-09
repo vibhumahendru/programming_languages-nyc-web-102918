5e80755ccc8729b22176b14c46d5e14562d17ff3
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-09 02:22:37 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -32,23 +32,32 @@ languages = {
 
 def reformat_languages(languages)
   new_hash = {}
-  style_type = [:oo, :functional]
-
   languages.each do |style, style_block|
     style_block.each do |language_name, type|
-      
       new_hash[language_name] = type
     end
   end
-  new_hash.each do |languageName, value|
-    if languageName == :ruby ||:python ||:java
-      new_hash[languageName] = value.merge(:style => [:oo])
-    else 
-      new_hash[languageName] = value.merge(:style => [:functional])
+  new_hash.each do |languageName, language_detail_hash|
+    if languageName == :python || :ruby || :java
+      language_detail_hash.store(:style, [:oo])
     end
   end
-end
-    
+  new_hash[:javascript] = {
+    :type => "interpreted",
+    :style => [:oo, :functional]
+  }
+    new_hash[:erlang]= {
+    :type => "compiled",
+    :style => [:functional]
+  }
+  new_hash[:scala]= {
+    :type => "compiled",
+    :style => [:functional]
+  }
+  new_hash[:clojure]= {
+    :type => "compiled",
+    :style => [:functional]
+  }
   new_hash
 end
 
